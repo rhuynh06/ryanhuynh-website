52c5194f0221a1d1f7075c7529a8b1261eb8e254
ryan huynh <ryanhuynh200604@gmail.com>
2025-07-18 15:53:58 -0700

    built docs

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss(),],
   base: "/ryanhuynh-website/",
   build: {
-    outDir: "../docs",
+    outDir: "./docs",
     emptyOutDir: true
   }
 })
