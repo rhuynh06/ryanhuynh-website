01f719ab28afd5773636ef24e89f0913e0a47b8f
ryan huynh <ryanhuynh200604@gmail.com>
2025-07-18 11:25:15 -0700

    converted project from plain HTML/CSS/JS to React

--- js/certifications.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const certificateCards = document.querySelectorAll('.certificate-card');
-
-    certificateCards.forEach(card => {
-        card.addEventListener('click', function() {
-            card.classList.toggle('flipped');
-        });
-    });
-});
