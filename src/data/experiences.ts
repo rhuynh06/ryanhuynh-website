141baee276a40df645636497d95c9f1582047561
ryan huynh <ryanhuynh200604@gmail.com>
2025-07-19 11:08:03 -0700

    added tooltip to show tech stack

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -4,13 +4,13 @@ export const experiences = [
       position: "Full-stack Web and AI Application Developer",
       dates: "June — Sept 2025",
       desc: `Led a team to build full-stack web and AI applications using React, Node.js, and Python. Integrated AI models into web apps using machine learning pipelines, enhancing interactivity. Developed real-time communication features using WebSocket technologies for responsive UX.`,
-      imageUrl: "experiences/dev_group.png"
+      imageUrl: "/experiences/dev_group.png"
    },
    {
       name: "KNT Foundation",
       position: "Organizing Committee & Volunteer",
       dates: "May 2019 — Present",
       desc: `Played a key role in planning and executing large-scale cultural and spiritual events such as Day of Vesak, with over 1,000 attendees. Coordinated logistics, collaborated with cross-functional teams, and developed leadership and organizational skills through hands-on service.`,
-      imageUrl: "experiences/knt.png"
+      imageUrl: "/experiences/knt.png"
    },
 ];
