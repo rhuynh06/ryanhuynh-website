fcd01481e08aadbfc11738f729a67d9af155406d
ryan huynh <ryanhuynh200604@gmail.com>
2025-08-25 11:29:07 -0700

    updated build

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -3,21 +3,21 @@ export const experiences = [
       name: "Independent Developer Group",
       position: "Full-stack Web and AI Application Developer",
       dates: "June — Sept 2025",
-      desc: `Led a team to build full-stack web and AI applications using React, Node.js, and Python. Integrated AI models into web apps using machine learning pipelines, enhancing interactivity. Developed real-time communication features using WebSocket technologies for responsive UX.`,
+      desc: `Built full-stack web and AI applications using React, Node.js, and Python. Integrated AI models and real-time communication features for interactive user experiences.`,
       imageUrl: "/experiences/dev_group.png"
    },
    {
       name: "Electronic Arts (Forage)",
       position: "SWE Virtual Experience Participant",
       dates: "August 2025",
-      desc: `Proposed and documented a new gameplay feature for EA Sports College Football, improving stakeholder communication. Designed class diagrams and implemented C++ header files for efficient object-oriented structure. Patched bugs and optimized codebase by applying improved data structures, enhancing performance and maintainability.`,
+      desc: `Proposed a new feature for EA Sports College Football, created class diagrams and C++ header files, and optimized the codebase with improved data structures.`,
       imageUrl: "/experiences/ea.svg"
    },
    {
       name: "KNT Foundation",
       position: "Organizing Committee & Volunteer",
       dates: "May 2019 — Present",
-      desc: `Played a key role in planning and executing large-scale cultural and spiritual events such as Day of Vesak, with over 1,000 attendees. Coordinated logistics, collaborated with cross-functional teams, and developed leadership and organizational skills through hands-on service.`,
+      desc: `Helped plan and execute large-scale cultural and spiritual events with 1,000+ attendees. Coordinated logistics and collaborated with teams to ensure smooth execution.`,
       imageUrl: "/experiences/knt.png"
    },
 ];
