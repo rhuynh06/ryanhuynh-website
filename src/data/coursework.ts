2b9c1c0e734bf070f46acd818d8686ccd82480a5
Ryan Do Huynh <huynhrd1@uci.edu>
2025-11-04 20:26:12 -0800

    updated coursework

--- src/data/coursework.ts
+++ src/data/coursework.ts
@@ -49,4 +49,97 @@ export const relatedCoursework = [
     code: "ICS 45C",
     link: "https://catalogue.uci.edu/allcourses/i_c_sci/"
   },
+  {
+    title: "Data Structure Implementation and Analysis",
+    code: "I&C SCI 46",
+    link: "https://catalogue.uci.edu/allcourses/i_c_sci/"
+  },
+  {
+    title: "Data Management",
+    code: "COMPSCI 122A",
+    link: "https://catalogue.uci.edu/allcourses/compsci/"
+  },
+  {
+    title: "Computer Organization",
+    code: "I&C SCI 51",
+    link: "https://catalogue.uci.edu/allcourses/i_c_sci/"
+  },
+/* winter 2026
+  {
+    title: "Machine Learning and Data-Mining",
+    code: "COMPSCI 178",
+    link: "https://catalogue.uci.edu/allcourses/compsci/"
+  },
+  {
+    title: "Design and Analysis of Algorithms",
+    code: "COMPSCI 161",
+    link: "https://catalogue.uci.edu/allcourses/compsci/"
+  },
+  {
+    title: "Principles in System Design",
+    code: "I&C SCI 53",
+    link: "https://catalogue.uci.edu/allcourses/i_c_sci/"
+  },
+  {
+    title: "Engineering Innovations in Treating Diabetes",
+    code: "BME 3",
+    link: "https://catalogue.uci.edu/allcourses/bme/"
+  },
+*/
+/* spring 2026
+  {
+    title: "Introduction to Artificial Intelligence",
+    code: "COMPSCI 171",
+    link: "https://catalogue.uci.edu/allcourses/compsci/"
+  },
+  {
+    title: "Human Computer Interaction",
+    code: "IN4MATX 131",
+    link: "https://catalogue.uci.edu/allcourses/in4matx/"
+  },
+  {
+    title: "Formal Languages and Automata",
+    code: "COMPSCI 162",
+    link: "https://catalogue.uci.edu/allcourses/compsci/"
+  },
+  {
+    title: "Project in Algorithms and Data Structures",
+    code: "COMPSCI 165",
+    link: "https://catalogue.uci.edu/allcourses/compsci/"
+  },
+*/
+/* fall 2027
+  // {
+  //   title: "Software Design: Applications",
+  //   code: "IN4MATX 121",
+  //   link: "https://catalogue.uci.edu/allcourses/in4matx/"
+  // },
+  // {
+  //   title: "Information Retrieval",
+  //   code: "COMPSCI 121",
+  //   link: "https://catalogue.uci.edu/allcourses/compsci/"
+  // },
+  // {
+  //   title: "Critical Writing on Information Technology",
+  //   code: "I&C SCI 139W",
+  //   link: "https://catalogue.uci.edu/allcourses/i_c_sci/"
+  // },
+*/
+/* winter 2027
+  {
+    title: "Project in Artificial Intelligence",
+    code: "COMPSCI 175",
+    link: "https://catalogue.uci.edu/allcourses/compsci/"
+  },
+  {
+    title: "User Interaction Software",
+    code: "IN4MATX 133",
+    link: "https://catalogue.uci.edu/allcourses/in4matx/"
+  },
+  {
+    title: "Next Generation Search Systems",
+    code: "COMPSCI 125",
+    link: "https://catalogue.uci.edu/allcourses/compsci/"
+  }
+*/
 ];
